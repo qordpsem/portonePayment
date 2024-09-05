d3f5da9937292c92448d4f12efd1ffab1c1ed2df
qordpsem <qordpsem@gmail.com>
2024-09-05 20:26:52 +0900

    [Feat] 게시물 수정, 삭제, 상세조회

--- src/main/java/com/example/portonepayment/controller/MerchandiseController.java
+++ src/main/java/com/example/portonepayment/controller/MerchandiseController.java
@@ -5,11 +5,12 @@ import com.example.portonepayment.entity.Merchandise;
 import com.example.portonepayment.repository.ClientRepository;
 import com.example.portonepayment.service.MerchandiseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.NoSuchElementException;
 
 
 @Controller
@@ -20,22 +21,41 @@ public class MerchandiseController {
     @Autowired
     private MerchandiseService merchandiseService;
 
-
     @GetMapping("/merchandise/list")
     public void list(Model model) {
         model.addAttribute("list", merchandiseService.findAll());
     }
 
     @GetMapping("/merchandise/insert")
-    @PreAuthorize("hasRole('admin')")
     public void insert(Model model) {
         model.addAttribute("merchandise", new Merchandise());
     }
 
     @PostMapping("/merchandise/insert")
-    @PreAuthorize("hasRole('admin')")
     public String insert(Merchandise merchandise) {
         merchandiseService.insert(merchandise);
+        return "redirect:/merchandise/list";
+    }
+
+    @GetMapping("/merchandise/update")
+    public void update(int num, Model model) {
+        model.addAttribute("m", merchandiseService.findByNUM(num));
+    }
+
+    @GetMapping("/merchandise/detail")
+    public String detail(int num, Model model) {
+        String view = "/merchandise/detail";
+        try {
+            model.addAttribute("m", merchandiseService.findByNUM(num));
+        } catch (NoSuchElementException e) {
+            view = "/error";
+            model.addAttribute("msg", "존재하지 않는 게시물");
+        }
+        return view;
+    }
+    @GetMapping("/merchandise/delete")
+    public String delete(int num) {
+        merchandiseService.delete(num);
         return "redirect:/merchandise/list";
     }
 }
--- src/main/java/com/example/portonepayment/service/MerchandiseService.java
+++ src/main/java/com/example/portonepayment/service/MerchandiseService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class MerchandiseService {
@@ -19,5 +20,12 @@ public class MerchandiseService {
 
     public void insert(Merchandise merchandise){
         merchandiseRepository.save(merchandise);
+    }
+
+    public Merchandise findByNUM(int num){
+        return merchandiseRepository.findById(num).orElseThrow(()-> new NoSuchElementException("없습니다" + num));
+    }
+    public void delete(int num){
+        merchandiseRepository.deleteById(num);
     }
 }
